87ac68a2961887c09adff1dd009dba9723c620eb
sezenteen <sezenteen@gmail.com>
2026-04-30 11:01:12 +0800

    base

---
+++ src/main/java/com/example/labwatch/LabWatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.labwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labwatch/LabWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
